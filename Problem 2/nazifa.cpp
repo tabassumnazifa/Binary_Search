cfcfa9cc7bdf4a50053de034e29b68e16fca100d
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2025-06-16 11:45:08 +0600

    Add files via upload

---
+++ Problem 2/nazifa.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       int it = lower_bound(nums.begin(), nums.end(), target)- nums.begin();
+        return it ;
+    }
+};
